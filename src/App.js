e6cc9ca3db36c8f7377e4266907e8cec1132526c
Justin Abellera <justin.c.abellera@gmail.com>
2022-07-30 19:20:14 -0700

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from "react";
+import './App.css';
+import { Link , Switch, Route, useParams} from 'react-router-dom'
 
 const App = () => {
   return (
     <>
+      <header>
+      <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      </div>
+      <nav className="nav-bar">
+        <Link className="link home" to='/'>Home</Link>
+        <img className="App-logo" src="https://flyclipart.com/thumb2/september-free-clipart-download-156740.png" alt='pizza-clip-art' />
+        <Link className="link order" to='/order-pizza'>Order</Link>
+      </nav>
+      </header>
+
+      <body>
+       <Switch>
+        <Route>
+
+        </Route> 
+       </Switch> 
+      </body>
+
+      <footer>
+      <nav className="nav-bar">
+        <Link className="link home" to='/'>Home</Link>
+        <Link className="link order" to='/order-pizza'>Order</Link>
+      </nav>
+      </footer>
     </>
   );
 };
